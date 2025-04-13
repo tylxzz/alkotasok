4d84865023ddc58a1a6e47cb80becfccd91a9fb1
tylxzz <markbencevarga@gmail.com>
2025-04-13 14:13:27 +0200

    div struktura siman

---
+++ main.js
@@ -0,0 +1,14 @@
+const div = (className) => {    // Ez egy arrow function, ami egy div elemet hoz létre a megadott className-nel
+    const div = document.createElement("div");  // Létrehoz egy div elemet
+    div.className = className;  // Beállítja a className-t
+    return div; // Visszaadja a létrehozott div elemet
+}
+
+const container = div("container"); // Letrehoz egy container div-et
+document.body.appendChild(container);   // hozzaadja a body-hoz
+const table = div("table"); // Letrehoz egy table div-et
+
+const form = div("form");   // Letrehoz egy form div-et
+
+container.appendChild(table);    // Hozzaadja a table div-et a container-hez
+container.appendChild(form);   // Hozzaadja a form div-et a container-hez
